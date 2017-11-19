c3f26b50545455282f3d0e9093ee85469071fd6e
ClaraAmeller <claraameller7@gmail.com>
2017-11-19 20:36:26 +0100

    Show categories, list things, show selected thing

--- routes/things.js
+++ routes/things.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-// const User = require('../models/user.js');
+const Category = require('../models/category');
+const Thing = require('../models/thing');
+const User = require('../models/user');
 
+// --- GET things by category --- //
 router.get('/things/:catId', (req, res, next) => {
   const catId = req.params.catId;
-  // res.render('auth/signup');
+  Thing.find({ categories: catId }, (err, thing) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('things/things_list', { data: thing });
+  });
+});
+
+// --- GET individual thing --- //
+router.get('/things/:id/show', (req, res, next) => {
+  const thingId = req.params.id;
+  Thing.findById(thingId, (err, thing) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('things/things_one', { thing });
+  });
 });
 
 module.exports = router;
