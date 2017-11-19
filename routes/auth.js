7329b0d366489973bf66530f8835f18ab35f8535
ClaraAmeller <claraameller7@gmail.com>
2017-11-19 12:35:34 +0100

    Fix Passport 1/2 bug

--- routes/auth.js
+++ routes/auth.js
@@ -72,7 +72,7 @@ router.post('/signup', (req, res, next) => {
       if (err) {
         res.render('auth/signup', { message: 'Something went wrong' });
       } else {
-        res.render('/');
+        res.redirect('/');
       }
     });
   });
