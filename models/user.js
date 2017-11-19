5c193bf3ff8b4493d9717ce12c4396373f53ce2c
ClaraAmeller <claraameller7@gmail.com>
2017-11-19 09:07:57 +0100

    Models/user.js + dependency Mongoose

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: String,
+    email: String,
+    telephone: String,
+    password: String,
+    prof_pic_path: String,
+    prof_pic_name: String
+}, {
+        timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+    });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
