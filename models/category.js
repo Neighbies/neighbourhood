41ea23bb33cd50384183501b5de9a4a69731fef5
Cristina Moreno <moreno.medran@gmail.com>
2017-11-22 14:56:16 +0100

    Add categories img

--- models/category.js
+++ models/category.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
-  title: String
+  title: String,
+  img_path = String,
+  img_name = String
 }, {
-  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-});
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+  });
 
 const Category = mongoose.model('Category', categorySchema);
 
-module.exports = Category;
+module.exports = Catesgory;
