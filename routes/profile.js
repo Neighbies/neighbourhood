fead5845f5a86f2d41fea352b1e919e84e960b86
ClaraAmeller <claraameller7@gmail.com>
2017-11-23 10:19:13 +0100

    Multer, localitzation to things_list

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../models/user');
+const User = require('../models/user');
 
 // --- GET home page --- //
 router.get('/profile', ensureAuthenticated, (req, res, next) => {
   res.render('profile/profile', {user: req.user});
+});
+
+// --- POST edit page --- //
+router.post('/profile/edit', (req, res, next) => {
+  console.log(req);
+  res.redirect('/');
 });
 
 function ensureAuthenticated (req, res, next) {
