4da7974d8cf0d154669389eb1379f5f1908e8190
ClaraAmeller <claraameller7@gmail.com>
2017-11-23 13:18:00 +0100

    My things

--- routes/mythings.js
+++ routes/mythings.js
@@ -6,15 +6,24 @@ const Thing = require('../models/thing');
 const User = require('../models/user');
 
 // --- GET all user things --- //
-router.get('/mythings', (req, res, next) => {
-  console.log(req.user._id);
-  //   Thing.find(findQuery, (err, thing) => {
-  //     if (err) {
-  //       return next(err);
-  //     }
-
-  res.render('things/mythings');
-//   });
+router.get('/mythings', ensureAuthenticated, (req, res, next) => {
+  Thing.find({user: req.user._id}, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    const data = {
+      things: result
+    };
+    res.render('things/mythings', data);
+  });
 });
+
+function ensureAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/login');
+  }
+}
 
 module.exports = router;
